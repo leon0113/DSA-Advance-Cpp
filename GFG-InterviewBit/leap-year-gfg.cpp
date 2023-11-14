89245cb6a9f8e9412940b6399bfa13564a0d6492
Leon <tahjib.leon399@gmail.com>
2023-11-14 12:11:39 +0600

    Leap year [GFG]

---
+++ GFG-InterviewBit/leap-year-gfg.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "Enter year: ";
+    cin >> year;
+    if (year % 400 == 0)
+    {
+        cout << "Leap Year" << endl;
+    }
+    else if (year % 4 == 0 && year % 100 != 0)
+    {
+        cout << "Leap Year" << endl;
+    }
+    else
+    {
+        cout << "Not a Leap Year" << endl;
+    }
+}
