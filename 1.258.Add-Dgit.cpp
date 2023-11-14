9ee081fc83319ef8f1c398e96b51abd54755852b
Leon <tahjib.leon399@gmail.com>
2023-11-14 11:54:05 +0600

    258

---
+++ 1.258.Add-Dgit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    while (num > 9)
+    {
+        int ans = 0, rem;
+        while (num != 0)
+        {
+            rem = num % 10;
+            num /= 10;
+            ans += rem;
+        }
+        num = ans;
+    }
+    cout << num;
+}
